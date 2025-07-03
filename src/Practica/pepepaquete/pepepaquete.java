0aedce9d2f1a57dcc6043bc5d12b896e8d7e9091
gIUTROLL <giutroll117troll@gmail.com>
2025-07-03 12:03:08 -0600

    Se crea el paquete pepepaquete y la clase pepepaquete
    Se crea el paquete con mi nombre y la clase con el mismo nombre, con un int y un string qu contiene ´hola´

---
+++ src/Practica/pepepaquete/pepepaquete.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practica.pepepaquete;
+
+/**
+ *
+ * @author Student
+ */
+public class pepepaquete {
+   public int Flore;
+   private String Texto = "hola";
+   
+   
+}
